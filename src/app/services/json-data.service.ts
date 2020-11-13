c2ec2752f5fe7f39a18766d5f8540074b548de0b
Fraser Rackham <fraser.rackham@speakersforschool.org>
2020-11-13 17:56:30 +0000

    Update with project deets.

--- src/app/services/json-data.service.ts
+++ src/app/services/json-data.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 import * as qvDimensions from 'src/data/example/current/qvDimensions.json';
 import * as qvEvidence from 'src/data/example/current/qvEvidence.json';
 import * as qvProject from 'src/data/example/current/qvProject.json';
+import * as qvSubProjects from 'src/data/example/current/qvSubProjects.json';
 import * as qvResults from 'src/data/example/current/qvResults.json';
 import { Dimension } from '../model/dimension';
 import { Evidence } from '../model/evidence';
@@ -15,9 +16,13 @@ import { Result } from '../model/result';
   providedIn: 'root'
 })
 export class JsonDataService {
+
+  //Use http://json2ts.com/ to create models from json files.
+
   dimensions:Dimension[];
   evidence:Evidence[];
   project:Project;
+  subProjects:any[]; //TODO: Need a model.
   results:Result[];
 
   constructor() {
@@ -25,14 +30,21 @@ export class JsonDataService {
     this.evidence = qvEvidence.evidence;
     this.project = qvProject.project;
     this.results = qvResults.results;
+
+    this.subProjects = qvSubProjects.subProjects;
   }
 
-  //Use http://json2ts.com/ to create models from the json.
+
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    console.log(this.dimensions)
+
+    // console.log(this.dimensions)
+    // console.log(this.evidence)
+    // console.log(this.project)
+    // console.log(this.results)
+    // console.log(this.subProjects)
   }
 
 
